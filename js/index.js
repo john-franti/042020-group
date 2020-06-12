426acdad4acb95c512c730eef69790a741ceab0f
John <Franti>
2020-06-12 15:15:34 -0700

    format site

--- js/index.js
+++ js/index.js
@@ -1,3 +1,19 @@
-document.addEventListener("DOMContentLoaded" () => {
-    alert("Welcome!")
-})
+const process = [
+  "Create a Production/Upstream Repo on GitHub",
+  "Each team member makes a FORK of that repo, and clones it locally",
+  "The local clone should have a `remote` added that points to the upstream",
+  "Team members should ALWAYS use a local feature branch to write new code",
+  "Before PUSHING to your fork, commit to FEATURE, checkout MASTER, then PULL UPSTREAM into MASTER",
+  "Then MERGE FEATURE into MASTER, and PUSH MASTER up to the FORK",
+  "FINALLY, create a PULL REQUEST, to send the fork to the UPSTREAM",
+  "...REPEAT as needed",
+];
+
+document.addEventListener("DOMContentLoaded", () => {
+  const list = document.getElementById("steps");
+  for (let text of process) {
+    const newStep = document.createElement("li");
+    newStep.textContent = text;
+    list.appendChild(newStep);
+  }
+});
